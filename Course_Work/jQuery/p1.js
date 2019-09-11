61b74532b830165626a9749bdac7ba1b23269a4c
LNPappas <LPappas@gmail.com>
2019-09-11 09:20:12 -0700

    project

--- Course_Work/jQuery/p1.js
+++ Course_Work/jQuery/p1.js
@@ -1,7 +1,5 @@
-var data =  $('h1')
-
-data.click(function(){
-  console.log('click!');
+ $('h1').click(function(){
+  $(this).text('clicked!');
 })
 
 
@@ -13,3 +11,17 @@ var newCSS = {
 }
 head.css(newCSS);
 $('input').eq(1).attr('type', 'checkbox');
+
+$('input').eq(0).keypress(function(event){
+  $('h2').toggleClass('turnBlue');
+  if (event.which === 13) {
+    $('h2').toggleClass('turnRed');
+  }
+})
+
+head.on('dblclick', function(){
+  $(this).toggleClass('turnBlue');
+})
+$('input').eq(1).on('click', function(){
+  $('.container').fadeOut(1000);
+})
